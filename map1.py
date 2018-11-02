4d72772ea701b866e2a64c3c28b1606a5437e166
Alex Cho <alexsjcho@gmail.com>
2018-11-01 20:16:26 -0700

    Choropleth Map

--- map1.py
+++ map1.py
@@ -4,14 +4,25 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lon = list(data["LON"])
 lat = list(data["LAT"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000<= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
 
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location= [lt, ln], popup="Hi I am your Marker",icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location= [lt, ln], radius = 6, popup =str(el)+" m", fill_color = color_producer(el), color = 'gray', fill_opacity= 0.7))
+
+fg.add_child(folium.GeoJson(data=open('world.json','r', encoding ='utf-8-sig').read(), style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005']< 10000000 else 'orange' if 10000000 <= x ['properties']['POP2005'] < 200000000 else 'red'}))
 
 map.add_child(fg)
 
